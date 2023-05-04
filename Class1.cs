8a34f6e60ac1dc34981f127a1038456cbd7878f2
Serhii Amelin <amserega90@gmail.com>
2023-05-04 18:09:05 +0300

    Add project files

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegats
+{
+    internal class Class1
+    {
+        public delegate void ShowHandler(bool isTrue);
+
+        public int Pow(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+namespace Delegats
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var class1 = new Class1();
+            var class2 = new Class2();
+            
+            class2.Calc(new Class2.PowHandler(class1.Pow), 10, 5, 2);
+
+            Class1.ShowHandler show;
+            show = Show;
+            show(Class2._resultDel);
+            
+        }
+
+        public static void Show(bool isTrue)
+        {
+            Console.WriteLine(isTrue);
+        }
+    }
+}
